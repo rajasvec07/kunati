f319136f9cfe0e0dd7d7675393fb07067547012d
rajasvec07 <rajasvec07@gmail.com>
2016-08-11 18:17:02 +0530

    Create messages.js

---
+++ app/messages/messages.js
@@ -0,0 +1,50 @@
+[
+    {
+        "from": 9848123413,
+        "text": "asdf asdf sd asdjkjas as djkasdnn jkhasdjkasd",
+        "date": "02/05/2016",
+        "image": "images/profile32x32.jpg"
+    },
+    {
+        "from": 9848123413,
+        "text": "asdf asdf sd asdjkjas as djkasdnn jkhasdjkasd",
+        "date": "02/05/2016",
+        "image": "images/profile32x32.jpg"
+    },
+    {
+        "from": 9848123413,
+        "text": "asdf asdf sd asdjkjas as djkasdnn jkhasdjkasd",
+        "date": "02/05/2016",
+        "image": "images/profile32x32.jpg"
+    },
+    {
+        "from": 9848123413,
+        "text": "asdf asdf sd asdjkjas as djkasdnn jkhasdjkasd",
+        "date": "02/05/2016",
+        "image": "images/profile32x32.jpg"
+    },
+    {
+        "from": 9848123413,
+        "text": "asdf asdf sd asdjkjas as djkasdnn jkhasdjkasd",
+        "date": "02/05/2016",
+        "image": "images/profile32x32.jpg"
+    },
+    {
+        "from": 9848123413,
+        "text": "asdf asdf sd asdjkjas as djkasdnn jkhasdjkasd",
+        "date": "02/05/2016",
+        "image": "images/profile32x32.jpg"
+    },
+    {
+        "from": 9848123413,
+        "text": "asdf asdf sd asdjkjas as djkasdnn jkhasdjkasd",
+        "date": "02/05/2016",
+        "image": "images/profile32x32.jpg"
+    },
+    {
+        "from": 9848123413,
+        "text": "asdf asdf sd asdjkjas as djkasdnn jkhasdjkasd",
+        "date": "02/05/2016",
+        "image": "images/profile32x32.jpg"
+    }
+]
